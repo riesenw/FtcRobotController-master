3eb98bfbcd791d2a8ee47846f58adec9b93f07e9
williamRiesen <riesenw@mercyhealth.com>
2024-01-10 16:15:03 -0600

    Red and blue autonomous now separated and function correctly more or less. Optimization needed.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousBlue.java
@@ -33,7 +33,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 
-@Autonomous(name = "Autonomous Blue", group = "Autonomous")
+@Autonomous(name = "Autonomous BLue", group = "Autonomous")
 
 public class AutonomousBlue extends LinearOpMode {
 
@@ -44,8 +44,86 @@ public class AutonomousBlue extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
-        AutonomousMaster.go(this, false);
+
+
+        // set constants specific to red or blue team assignment: blue values are the default
+
+
+        double TOWARD_AUDIENCE = -90;
+        double AWAY_FROM_AUDIENCE = 90;
+        double TOWARD_DRIVERS = 180;
+        double AWAY_FROM_DRIVERS = 0;
+        double ANGLE_TOWARD_BACK_CENTER = 0;
+
+
+
+
+        RobotHardware robot = new RobotHardware(this);
+        robot.init();
+
+        waitForStart();
+
+        robot.strafe(AWAY_FROM_AUDIENCE,6,0.1);
+        boolean objectDetected =
+                robot.forward(AWAY_FROM_DRIVERS, 28, 0.1);
+        if(objectDetected)
+
+        {
+            //We now know randomization was to the RIGHT position.
+            robot.deployPixel();
+            robot.backUp(TOWARD_DRIVERS, 9, 0.1);
+            sleep(500);
+            robot.rotateToHeading(AWAY_FROM_AUDIENCE);
+            sleep(500);
+
+            robot.forward(AWAY_FROM_AUDIENCE, 27, 0.1);
+            sleep(1000);
+//            robot.strafe(AWAY_FROM_DRIVERS,8, 0.1);
+//            myOpMode.sleep(3000);
+
+//            boolean targetReached = robot.autoDriveToTarget(6);
+            // strafe two inches to the right to correct for camera not centered on robot
+
+//            myOpMode.sleep(3000);
+
+//            robot.strafeRight(robot.getHeading()+ 90, 2.0, 0.1);
+//            myOpMode.sleep(1000);
+            robot.strafeLeft(AWAY_FROM_DRIVERS, 1., 0.1);
+            robot.moveArmToFlipPosition();
+//            myOpMode.telemetry.addData("Target Reached", targetReached);
+//            myOpMode.telemetry.update();
+            sleep(3000);
+            robot.moveArmToCarryPosition();
+            sleep(1000);
+            robot.backUp(TOWARD_AUDIENCE, 1, 0.1);
+            robot.strafeRight(TOWARD_DRIVERS, 14, 0.2);
+            robot.forward(AWAY_FROM_AUDIENCE, 5, 0.1);
+
+
+        } else
+
+        {
+            robot.backUp(TOWARD_DRIVERS, 14, 0.1);
+            sleep(500);
+            robot.strafeLeft(TOWARD_AUDIENCE, 8, 0.1);
+            sleep(500);
+            objectDetected =
+                    robot.forward(AWAY_FROM_DRIVERS, 22, 0.1);
+            sleep(500);
+            if (objectDetected) {
+                // We now know that the randomization was to center
+                robot.deployPixel();
+                robot.backUp(TOWARD_DRIVERS, 2, 0.1);
+
+            } else {
+                robot.backUp(TOWARD_DRIVERS, 6.5, 0.1);
+                sleep(500);
+                robot.strafeLeft(TOWARD_AUDIENCE, 8, 0.1);
+                robot.deployPixel();
+                robot.backUp(TOWARD_DRIVERS, 1.5, 0.1);
+            }
+        }
+        sleep(30000);
+
     }
 }
-
-
